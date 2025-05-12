cef3d89e1f1373d3cf1bf66cb9f3a8aa0e6a5c77
GigikerJD <nelgie.ferrol94@gmail.com>
2025-05-12 19:24:26 +0100

    update db from composite primary keys

--- src/main/java/com/project/core/mysql/resources/MovieResource.java
+++ src/main/java/com/project/core/mysql/resources/MovieResource.java
@@ -1,8 +1,6 @@
 package com.project.core.mysql.resources;
 
-import com.project.core.mysql.entities.Actor;
 import com.project.core.mysql.entities.Jouer;
-import com.project.core.mysql.entities.JouerId;
 import com.project.core.mysql.entities.Movie;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -26,55 +24,30 @@ public class MovieResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMovies() {
         var movies = em.createQuery("select m from Movie m").getResultList();
+        Map<String, Object> response = new HashMap<>();
         if (movies.isEmpty()) {
-            Map<String, Object> response = new HashMap<>();
             response.put("message", "No movies found");
-            return Response.status(Response.Status.NOT_FOUND).entity(response).build();
+            response.put("status", 404);
+            return Response.ok(response).build();
         }
+        response.put("message", "Found " + movies.size() + " movies");
+        response.put("status", 200);
         return Response.ok(movies).build();
     }
 
+    /*
     @GET
     @Path("/{title}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMovie(@PathParam("title") String title) {
-        var movie = em.find(Movie.class, title);
-        if(movie == null) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("message", "Movie not found");
-            return Response.status(Response.Status.NOT_FOUND).entity(response).build();
-        }
 
-        var jouers = em.createQuery("select j from Jouer j where j.title.title = :title", Jouer.class)
-                .setParameter("title", title)
-                .getResultList();
-
-        var actors = new ArrayList<Actor>();
-        for (Jouer jouer : jouers) {
-            actors.add(jouer.getId1());
-        }
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Movie found");
-        response.put("actors", actors);
-        response.put("movie", movie);
-        return Response.ok(response).build();
     }
 
     @GET
     @Path("/city")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMovieByCity(@QueryParam("name") String cityName) {
-        var moviesByCity = em.createQuery("select m from Movie m where m.city = :cityName", Movie.class)
-                .setParameter("cityName", cityName)
-                .getResultList();
 
-        if (moviesByCity.isEmpty()) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("message", "No movies found for city: " + cityName);
-            return Response.status(Response.Status.NOT_FOUND).entity(response).build();
-        }
-
-        return Response.ok(moviesByCity).build();
     }
+    */
 }
